5390f867324402512963abe3d25748e136ce48f5
Jai <mail@mail.com>
2024-05-29 11:25:46 +1000

    First Commit Getting my Password Checker to reconignse common passwords

---
+++ Pie.py
@@ -0,0 +1,81 @@
+import gooeypie as gp
+
+from list_of_common_passwords import common_passwords 
+def password_list():
+    password_list = common_passwords
+
+def on_text_change(event):
+    text = text_box.text
+    print(text)
+
+    if text == "Jai":
+        label.text = "Me"
+    elif text == "Gus":
+        label.text = "Toothbrush"
+    elif text == password_list():
+        label.text = "Your Password is to common"
+    else:
+        label.text = "Your password is unique"
+    elif len(text) == >= 8:
+        label.text = "Your password is strong"
+    else:
+        label.text = "Needs More Words Champ"
+   
+
+    
+    
+app = gp.GooeyPieApp('Password Checker')
+app.width = 600
+app.height = 500
+text_box = gp.Textbox(app, 60, 10)
+text_box.add_event_listener('change', on_text_change)
+label = gp.Label(app, 'blank')
+
+app.set_grid(2,1)
+app.add(text_box, 1, 1)
+app.add(label, 2, 1)
+app.run()
+
+
+# char_num = -
+# symbol_detected = Falsesymbols = [@#$%^&*]
+# while symbol_detected == False and char_num <= length of List_of_Common_Passwords
+# #Check each character agaaninst the symbols list
+# For I = 0 to length of symbols
+# If password[char_num] == symbols[i]
+# symbol_detected = True
+# next i
+# char_num +=
+
+
+
+
+# import gooeypie as gp
+
+# from list_of_common_passwords import common_passwords 
+
+
+# password_list = common_passwords
+
+# def on_text_change(event):
+#     text = text_box.text
+#     if text in password_list:
+#         label.text = "Your password is too common"
+#     elif len(text) >= 8:
+#         label.text = "Your password is strong"
+#     else:
+#         label.text = "Needs More Words Champ"
+
+# app = gp.GooeyPieApp('Password Checker')
+# app.width = 600
+# app.height = 500
+
+# text_box = gp.Textbox(app, 60, 10)
+# text_box.add_event_listener('change', on_text_change)
+
+# label = gp.Label(app, 'blank')
+
+# app.set_grid(2, 1)
+# app.add(text_box, 1, 1)
+# app.add(label, 2, 1)
+# app.run()
