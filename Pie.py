acd6924b429c926acc50416b3bce71b51ef4b6fd
Jai <mail@mail.com>
2024-06-03 09:31:18 +1000

    Now Detects Symbols and Numbers

--- Pie.py
+++ Pie.py
@@ -13,7 +13,6 @@
 
 
 
-
 import gooeypie as gp
 
 from list_of_common_passwords import common_passwords 
@@ -23,27 +22,31 @@ common_passwords = common_passwords()
 # for password in common_passwords:
 #     print(password)
 
+
+
+import gooeypie as gp
+
 def on_text_change(event):
     text = text_box.text
+    invalid_symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "~", "`", "-", "_"}
+    numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+ 
     if text in common_passwords:
         label.text = "Your password is too common"
+    elif any(character in invalid_symbols for character in text):
+        label.text = "Your password has a symbol"
+    elif any(character in numbers for character in text):
+        label.text = "Your password has a number"
     elif len(text) >= 8:
         label.text = "Your password is strong"
     else:
         label.text = "Needs More Words Champ"
-    
-    
-
-invalid_symbols = {"!,", "@", "#", "$", "%", "^", "&", "*", "~", "`", "-", "_"}
-if any(character in invalid_symbols for character in text):
-    message +- "No symbols are allaowed in password./n"
-
 
 app = gp.GooeyPieApp('Password Checker')
 app.width = 600
 app.height = 500
 
-text_box = gp.Textbox(app, 60, 10)
+text_box = gp.Textbox(app, 60)
 text_box.add_event_listener('change', on_text_change)
 
 label = gp.Label(app, 'blank')
@@ -51,4 +54,8 @@ label = gp.Label(app, 'blank')
 app.set_grid(2, 1)
 app.add(text_box, 1, 1)
 app.add(label, 2, 1)
+
+
 app.run()
+
+
