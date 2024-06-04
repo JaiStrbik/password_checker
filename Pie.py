54cec2dc999794bd12811cd28ea006be717364f0
Jai <mail@mail.com>
2024-06-04 17:59:57 +1000

    making more progress on PassWord Generator button

--- Pie.py
+++ Pie.py
@@ -15,7 +15,7 @@
 
 import gooeypie as gp
 from list_of_common_passwords import common_passwords
-
+from random import choice
 common_passwords = common_passwords()
 
 def on_text_change(event):
@@ -37,23 +37,26 @@ def on_text_change(event):
 def check_password(event):
     on_text_change(event)
 
-app = gp.GooeyPieApp('Password Checker')
-app.width = 600
-app.height = 500
 
-text_box = gp.Textbox(app, 60)
-text_box.add_event_listener('change', on_text_change)
-
-check_button = gp.Button(app, 'Click Here for a Strong Password', check_password)
-
-label = gp.Label(app, 'blank')
 
 def generate_password(length, letters, digits, symbols):
     """returns a new password within a 10 out of 10 ranked password upon clicked on the button"""
     #set characters from which to choose from
     #Make the password by using the string above
- 
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    available_chars = ''
+    if letters:
+        available_chars
+    if digits:
+        available_chars
+    if symbols:
+        available_chars
+    #Make the new password by choosing from the avialble letters,digits and symbols above
+    new_password = ''
+    for count in range(length): #For Loop
+        new_password += choice(available_chars)
+    return new_password
+    
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 letters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
@@ -67,11 +70,26 @@ symbols = [
     '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_',
     '`', '{', '|', '}', '~'
 ]
+length = int(8)
+def show_new_password(event):
+    """Populates the password box with a newly generated password"""
+    gp.Textbox= generate_password(length, letters, symbols, digits)
 
 # Print the sections
-print("Numbers:", numbers)
-print("Letters:", letters)
-print("Symbols:", symbols)
+
+app = gp.GooeyPieApp('Password Checker')
+app.width = 600
+app.height = 500
+check_button = gp.Button(app, 'Click Here for a Strong Password', show_new_password)
+
+
+text_box = gp.Textbox(app, 60)
+text_box.add_event_listener('change', on_text_change)
+
+
+
+label = gp.Label(app, 'blank')
+
 
 
 app.set_grid(3, 1)
