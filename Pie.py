cba461f4025b6987a3c27a8e749bc0172d6003a9
Jai <mail@mail.com>
2024-06-26 21:02:43 +1000

    images with buttons are in and separate windows with information about me and about the app is done.

--- Pie.py
+++ Pie.py
@@ -1,7 +1,6 @@
 
 
 
-
 import gooeypie as gp
 from list_of_common_passwords import common_passwords
 from random import choice
@@ -106,7 +105,34 @@ def show_new_password(event):
 def toggle_mask(event):
     text_box.secret = not text_box.secret
 
-# Character sets
+
+
+
+def show_question_mark_info(event):
+    show_info(event, """
+The construction of SaintLock is as follows: 
+I defined lowercase letters, uppercase letters, numbers, digits with a text box. 
+Next, I set a points system. If any of the defining characters in each defined list are in the text box, points will be added. 
+With extra variables of length and the time it takes to crack the password to add points to a ranking. 
+If your password doesn't meet the perfect 10/10 ranking, feedback will be given to improve your password. 
+If you don't want to write a password, a password generator is added to give you a perfect password 
+which you can copy to your clipboard at any time. 
+Thanks for using my app and keep your personal information safe!""")
+
+def show_information_info(event):
+    show_info(event, """
+I am a 16-year-old computer scientist at Saint Augustine's College, Sydney, who has just begun my coding journey this year. 
+I have created this app, SaintLock, to protect your personal information from hackers in this ever-changing technological world.""")
+
+def show_info(event, info_text):
+    """Creates a new window with the provided information text"""
+    info_window = gp.GooeyPieApp('Information')
+    info_label = gp.Label(info_window, info_text)
+    info_window.set_grid(1, 1)
+    info_window.add(info_label, 1, 1, align='center', valign='middle')
+    info_window.run()
+
+
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 letters = [
@@ -124,9 +150,9 @@ symbols = [
 length = 16
 
 # GooeyPie App setup
-app = gp.GooeyPieApp('Password Checker')
-app.width = 600
-app.height = 500
+app = gp.GooeyPieApp('SaintLock')
+app.width = 850
+app.height = 600
 
 text_box_label = gp.Label(app, 'Enter Password:')
 text_box = gp.Secret(app)
@@ -136,7 +162,7 @@ text_box.add_event_listener('change', on_text_change)
 check = gp.Checkbox(app, 'Show Password')
 check.add_event_listener('change', toggle_mask)
 
-check_button = gp.Button(app, 'Generate Password', show_new_password)
+generate_button = gp.Button(app, 'Generate Password', show_new_password)
 copy_button = gp.Button(app, 'Copy to Clipboard', copy_to_clipboard)
 label = gp.Label(app, 'Password Score: 0')
 ranking_label = gp.Label(app, 'Ranking: 0/10')
@@ -145,14 +171,17 @@ crack_times_label = gp.Label(app, 'Time to Crack: N/A')
 feedback_label = gp.Label(app, 'Feedback:')
 feedback = gp.Label(app, '')
 
-question_mark_button = gp.ImageButton(app, 'Q1.png', None, '')
-Information_Button = gp.ImageButton(app, 'i1.png', None,'')
+question_mark_button = gp.ImageButton(app, 'q10.png', None, '')
+information_button = gp.ImageButton(app, 'i1.png', None, '')
+
+question_mark_button.add_event_listener('press', show_question_mark_info)
+information_button.add_event_listener('press', show_information_info)
 
 app.set_grid(10, 3)
 app.add(text_box_label, 1, 2, align='center', valign='middle')
 app.add(text_box, 2, 2, align='center', valign='middle')
 app.add(check, 3, 2, align='center')
-app.add(check_button, 4, 2, align='center')
+app.add(generate_button, 4, 2, align='center')
 app.add(label, 5, 2, align='center')
 app.add(ranking_label, 6, 2, align='center')
 app.add(crack_times_label, 7, 2, align='center')
@@ -160,5 +189,15 @@ app.add(feedback_label, 8, 2, align='center')
 app.add(feedback, 9, 2, align='center')
 app.add(copy_button, 10, 2, align='center')
 app.add(question_mark_button, 10, 3, align='center')
-app.add(Information_Button, 10, 1, allign='center')
+app.add(information_button, 10, 1, align='center')
+
 app.run()
+
+
+
+
+
+
+
+
+
